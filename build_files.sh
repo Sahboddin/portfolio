3c8247829a98de9b3c6d6ee27510c2add275e223
sahboddin <mdsahboddink@gmail.com>
2025-01-26 19:35:55 +0600

    bal3

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,3 @@
 pip install -r requirements.txt
-<<<<<<< HEAD
 Python 3.10 manage.py collectstatic
-=======
-Python 3.10.9 manage.py collectstatic
->>>>>>> 9856ac00feb1361090fe4369c77ff35542d0ef5a
+
